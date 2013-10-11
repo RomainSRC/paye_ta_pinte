ecdef2d4823bc1ec1237903fe40bf6ca4a6c7686
PqBenoit <benoit@monsieurdrive.com>
2013-10-11 12:00:01 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PayeTaPinte::Application.config.secret_token = 'e9f25d47aab4be8bda9925e9c6df0ca6c76a39a1e198a1ac8ded0e3ced5d3d68626ef1161df1afeb353121d16026e2d145003f1e2b8f09d625eeae8f5a15322b'
